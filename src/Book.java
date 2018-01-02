fbc8bbe73efea3ed0adbb4bc28eb5d19328031ec
tsc <tsc@tan>
2018-01-02 23:09:19 +0800

    hello

---
+++ src/Book.java
@@ -0,0 +1,10 @@
+
+public class Book {
+	private String Code;
+	private String BookName;
+	private int BookNumber;//数量
+	private int StoreNumber;//在馆数量
+	private String Location;
+	private String Author;
+	private String Publication;
+}
