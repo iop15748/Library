f30b935be280e1a19e175e2f11366a4532507f61
tsc <tsc@tan>
2018-01-03 12:57:23 +0800

    123

---
+++ src/User.java
@@ -0,0 +1,10 @@
+
+public class User {
+	private String Code;
+	private String Password;
+	private String Name;
+	private String Class;//班级
+	private int Credit;//信用积分
+	private int BookNumber;//在借书目
+	private String BorrowBook[];//所借的书
+}
